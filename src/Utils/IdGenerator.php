e29224598997bf0310e72b47b742e24dc13068e1
Anton Melnikov <anton@melnikov.it>
2024-10-24 15:13:24 +0200

    commit

--- src/Utils/IdGenerator.php
+++ src/Utils/IdGenerator.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Incubatrix\Core\Utils;
 
+use Random\RandomException;
 use Symfony\Component\Uid\Uuid;
 
 final readonly class IdGenerator
@@ -11,5 +12,13 @@ final readonly class IdGenerator
     public static function id(): string
     {
         return Uuid::v7()->toRfc4122();
+    }
+
+    /**
+     * @throws RandomException
+     */
+    public static function token(): string
+    {
+        return bin2hex(random_bytes(32));
     }
 }
